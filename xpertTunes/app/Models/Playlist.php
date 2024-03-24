8db539fa130c325d1ddd1eea4c253ef75840e02b
Hadi Aldhaywi <aldhaywihad@gmail.com>
2024-03-25 00:10:14 +0200

    Adding functions to playlist model

--- xpertTunes/app/Models/Playlist.php
+++ xpertTunes/app/Models/Playlist.php
@@ -14,4 +14,11 @@ class Playlist extends Model
         'user_id',
     ];
 
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function songs(){
+        return $this->belongsToMany(Song::class);
+    }
 }
